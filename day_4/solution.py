c924c7a54e3174bbea5ea0bfebe42413d2eca073
khie <khie@thoughtmachine.net>
2020-12-04 17:32:33 +0000

    finish day 4

---
+++ day_4/solution.py
@@ -0,0 +1,62 @@
+input_file = open("input.txt", "r").read()
+
+entries = input_file.split('\n\n')
+passports = [[y for y in x if y != ''] for x in [x.replace('\n', ' ').split(' ') for x in entries]]
+
+def validate_fields(required, seen, optional):
+    difference = required.difference(seen)
+    ans = difference.issubset(optional)
+    return(ans)
+
+def validate_values(key, value):
+    if key == 'cid':
+        return True # we don't need to validate this
+    if key == 'byr':
+        value_as_num = int(value)
+        return value_as_num >= 1920 and value_as_num <= 2002
+    if key == 'iyr':
+        value_as_num = int(value)
+        return value_as_num >= 2010 and value_as_num <= 2020
+    if key == 'eyr':
+        value_as_num = int(value)
+        return value_as_num >= 2020 and value_as_num <= 2030
+    if key == 'hgt':
+        front = value[:-2]
+        back = value[-2:]
+        if front.isnumeric() and back in ['cm','in']:
+            front = int(front)
+            if back == 'cm':
+                return front >= 150 and front <= 193
+            else:
+                return front >= 59 and front <= 76
+    if key == 'hcl':
+        if value[0] == '#':
+            count = 0
+            valid = True
+            for letter in value[1:]:
+                count += 1
+                valid = letter.isnumeric() or letter in ['a','b','c','d','e','f']
+            return valid and len(value[1:]) == 6
+    if key == 'ecl':
+        return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    if key == 'pid':
+        return len(value) == 9 and value.isnumeric()
+    return False
+
+
+required = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+optional = {'cid'}
+
+valid_count = 0
+
+for passport in passports:
+    seen = set()
+    valid_values = True
+    for item in passport:
+        key, value = tuple(item.split(':'))
+        valid_values = valid_values and validate_values(key, value)
+        seen.add(key)
+    if valid_values and validate_fields(required, seen, optional):
+        valid_count += 1
+
+print(valid_count)
